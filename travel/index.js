69e66b5aa72b1c4b72536c4ffbafd6ba8f53ff11
Pavel <whatisclamp@gmail.com>
2022-06-30 16:03:13 +0300

    feat: Add opacity overlay after burger menu open

--- travel/index.js
+++ travel/index.js
@@ -5,26 +5,29 @@ let burgerClose = document.querySelector('.burger_close_icon');
 let burgerMenu = document.querySelector('.header_menu_list');
 let header = document.querySelector('header')
 let menuList = document.querySelector('.header_menu_list');
-let menuLinks = document.querySelectorAll('.header_menu_list')
+let menuLinks = document.querySelectorAll('.header_menu_list');
+let overlay = document.querySelector('.overlay');
 
 
 burgerClose.addEventListener('click', () => {
     burgerMenu.classList.remove('show');
+    overlay.classList.remove('show')
 })
 
 menuLinks.forEach(link => {
     link.addEventListener('click', () => {
         burgerMenu.classList.remove('show');
+        overlay.classList.remove('show')
     })
 })
 
 document.addEventListener('click', (e) => {
     if (e.target.classList.contains('burger_icon')) {
         burgerMenu.classList.add('show');
+        overlay.classList.add('show')
     } else if (!e.target.classList.contains('header_menu_list')) {
         burgerMenu.classList.remove('show');
-    } else if (e.target.classList.contains('header_menu_list')) {
-        burgerMenu.classList.add('show');
+        overlay.classList.remove('show')
     }
 })
 
